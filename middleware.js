2af4fcb878d9797441416b625047428438aba2aa
aleksandr gilfanov <pim_pam@mail.ru>
2014-05-16 13:49:10 +0600

    added middleware example application

---
+++ middleware.js
@@ -0,0 +1,22 @@
+// Require the stuff we need
+var express = require("express");
+var http = require("http");
+
+// Build the app
+var app = express();
+
+// Add some middleware
+app.use(function(request, response) {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+
+  var answer = "";
+  answer += "Request URL: " + request.url + "\n";
+  answer += "Request type: " + request.method + "\n";
+  answer += "Request headers: " + JSON.stringify(request.headers) + "\n";
+  response.write(answer);
+
+  response.end("\nHello world!\n");
+});
+
+// Start it up!
+http.createServer(app).listen(1337);
